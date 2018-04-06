7741bcfb991349e80e2bde0b8c24cd55e329547c
Mikael Brochu <mikael.brochu@gmail.com>
2018-04-06 11:13:49 -0400

    inputs, deathzone

---
+++ space_jam/Assets/DeathZoneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZoneTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+
+        ////OLD POLAR BEAR CODE
+        //if (other.CompareTag("Player"))
+        //{
+        //    //GetComponent<Animator>().SetTrigger("EatingRange");
+        //    //other.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+        //    bearManager.PlayOneShot(snapSound);
+        //    StartCoroutine(WaitAndHideMesh(other.gameObject, fishDissapearDelay));
+        //}
+    }
+}
